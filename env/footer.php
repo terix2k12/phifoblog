dc4e026978bb8877445b4b9ea7fb2d844a2ca28a
terix2k12 <terixmail@googlemail.com>
2018-09-02 18:39:55 +0200

    Add LICENSE

--- env/footer.php
+++ env/footer.php
@@ -1,6 +1,6 @@
 		<div id='footer'>
 			<a href='env/imprint_en.php' id='imprint'>Imprint</a>
-			<p id='copy'>&copy; 2018 Philipp Fonteyn</p>
+			<p id='copy'>&copy; 2018 <a class="logon" href="article.php">Philipp Fonteyn</a></p>
 		</div>
 	</body>
 </html>
