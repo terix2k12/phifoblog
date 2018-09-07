c5e08575b6467a868351c80e26d3501d91a09e0d
terix2k12 <terixmail@googlemail.com>
2018-09-07 21:30:51 +0200

    New articles with prepared SQL statements

--- article.php
+++ article.php
@@ -3,36 +3,46 @@
 	
 	include("dbcredentials.php");	
 
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	// $conn = mysqli_connect();
+
+	$mysqli = new mysqli($servername, $username, $password, $dbname);
+	if ($mysqli->connect_errno) {
+    	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
 ?>
 
 <div id="contentcontainer">
 
 <?php
-	$password = htmlspecialchars($_POST['pass']);
-	$name = mysqli_real_escape_string($conn, htmlspecialchars($_POST['name']));
-	$content = mysqli_real_escape_string($conn, htmlspecialchars($_POST['content']));
+	$password = $_POST['pass'];
+	$name = $_POST['name'];
+	$content = $_POST['content'];
 
-	if(!empty($password)){
-		if($password == $submissionpwd){
+	if(!empty($password) && $password == $submissionpwd){
 
 			if(!empty($name) && !empty($content)){
 
-				$sqlPut = "INSERT INTO ARTICLES (id, name, content) VALUE (0,'".$name."','".$content."');";
-				// $response = mysqli_query($conn, $sqlPut);
-				// ignore sql statement for now :)
-
-				if ($response === TRUE) {
-					 echo "Data processed.\n";
-				} else {
-					echo "Data failure.";  
+				if (!($stmt = $mysqli->prepare("INSERT INTO ARTICLES (id, name, content) VALUE (0,?,?)"))) {
+				    // echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
 				}
+
+				if (!$stmt->bind_param("ss", $name, $content)) {
+				    // echo "Binding parameters 1 failed: (" . $stmt->errno . ") " . $stmt->error;
+				}
+				
+				if (!$stmt->execute()) {
+				    // echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+				     echo "Data failure.\n";
+				}else{
+					 echo "Data processed.\n";
+				}
+
 			}
 
-		}else{
-			echo "Wrong password!";
-		}
+	}else{
+		echo "Wrong password!";
 	}
+
 ?>
 
 <form action="https://www.philippfonteyn.de/content/article.php" method="post">
@@ -48,7 +58,7 @@
 	<ul>
 	<?php
 		$sqlGet = 'SELECT * FROM ARTICLES;';
-		$response = mysqli_query($conn, $sqlGet);
+		$response = mysqli_query($mysqli, $sqlGet);
 		    if (mysqli_num_rows($response) > 0) {	
 		    	while($row = mysqli_fetch_assoc($response)) {
 		    		$rowname = htmlspecialchars($row["name"]);
@@ -64,5 +74,5 @@
 
 <?php
 	include("env/footer.php");
-	mysqli_close($conn);
+	mysqli_close($mysqli);
 ?>
