0828456e456c92d2889e42914fafb538148f3a24
terix2k12 <terixmail@googlemail.com>
2018-10-28 15:20:54 +0100

    Basic article mechanism

--- env/article.php
+++ env/article.php
@@ -102,9 +102,49 @@
 
 					if (!$stmt->execute()) {
 					    // echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-					     echo "Data failure.\n";
+					     echo "Data failure.<br>\n";
 					}else{
-						 echo "Data processed.\n";
+						 echo "Data processed.<br>\n";
+
+						if($nid==-1){
+						  	$nid = mysqli_insert_id($mysqli);
+						}
+
+	$categories = explode(",",htmlspecialchars($_POST['categories']));
+	$stmt = $mysqli->prepare("INSERT INTO ART_CAT (article, category) VALUE (?,?)");
+	$stmt->bind_param("ii", $nid, $catId);
+	
+	$selectCategoryId = $mysqli->prepare("SELECT ID FROM CATEGORY WHERE NAME = ?");
+	$selectCategoryId->bind_param("s", $ncat);
+	
+	$stmt2 = $mysqli->prepare("INSERT INTO CATEGORY (name) VALUE (?)");
+	$stmt2->bind_param("s", $ncat);
+
+	echo "SCHLEIFE ".var_dump($categories);
+
+	foreach($categories as $item) {
+		$ncat = trim($item);
+		$selectCategoryId->execute();
+
+		$res77 = $selectCategoryId->get_result();
+		$catId = $res77->fetch_row()[0];
+
+		echo "Process category ".$ncat."<br>\n";
+
+		if(empty($catId)){
+			$stmt2->execute();
+			$catId = mysqli_insert_id($mysqli);
+			echo "Category ".$ncat." created with Id ".$catId."<br>\n";
+		} else { 
+			echo "Category ".$ncat." found with Id ".$catId."<br>\n";
+		}
+
+		if (!$stmt->execute()) {
+			// echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+			echo "Category ".$ncat." failure.<br>\n";
+		} 
+	}
+
 					}
 				}else{
 					echo "Wrong password!";
@@ -127,8 +167,8 @@
 			}else{
 				echo htmlspecialchars($contentId); 
 			}
-			?>" /></p>
-			<p>Article: <input type="text" name="name" value="
+			?>" />
+			Article: <input type="text" name="name" value="
 <?php
 	if(!array_key_exists($contentId,$content)){
 		echo "New article";
@@ -146,7 +186,7 @@
 	}
 ?>
 			" /></p>
-			<!--<p>Category: <input type="text" name="category" /></p>-->
+			<p>Categories: <input type="text" name="categories" /></p>
  			<textarea name="content" rows="20" cols="100">
 <?php
 	if(!array_key_exists($contentId,$content)){
@@ -158,8 +198,8 @@
 	mysqli_close($mysqli);
 ?>
  			</textarea>
- 			<p>Delete:<input type="checkbox" name="delete"/></p>
- 			<p>Active:<input type="checkbox" name="active" <?php 
+ 			<p>Delete:<input type="checkbox" name="delete"/>
+ 			Active:<input type="checkbox" name="active" <?php 
 				if(!array_key_exists($contentId,$content)){
 					echo "checked";
 				}else{
