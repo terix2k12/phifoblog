f28dcf8a22e7f76a132925433a9105821a7a4a57
terix2k12 <terixmail@googlemail.com>
2020-05-23 16:18:42 +0200

    use thin dblayer in export

--- env/export.php
+++ env/export.php
@@ -1,36 +1,32 @@
 <?php
-	include("../dbcredentials.php");
+	include("../env/blogdatabase.php");
 	
 	$contentId = intval($_GET["id"]);
 
-	$mysqli = new mysqli($servername, $username, $dbpassword, $dbname);
-	
+	$db = new BlogDatabase("../dbcredentials.php");
+	$db->connect();
+
 	if(empty($contentId)) {
-		$stmt = $mysqli->prepare("SELECT * FROM ARTICLES;");
+		$outp = $db->getAllArticles();
 	} else {
-		$stmt = $mysqli->prepare("SELECT * FROM ARTICLES WHERE ID = ?;");
-		$stmt->bind_param("i", $contentId);
+		$outp = $db->getArticle($contentId);
 	}
 
-	// TODO generic database interaction
 	// TODO generic error handling
 	// TODO authorization
 	// TODO categories
 	// TODO art cat
 	// TODO date
 						
-	if (!$stmt->execute()) {
+	if (!$outp) {
 		echo "<h1>404 Not Found</h1>\n</body>\n</html>\n";
     	header("HTTP/1.0 404 Not Found");
-	} else {		 
-		$result = $stmt->get_result();
-		$outp = $result->fetch_all(MYSQLI_ASSOC);
-
+	} else {
 		$outpq = '{"exported": "2020-05-10",'.'"articles":'.json_encode($outp).', "categories": [], "art_cat":[]}';		
 
 		header("Content-Type: application/json; charset=UTF-8");
 		echo $outpq;		     
 	}
 
-	mysqli_close($mysqli);
+	$db->disconnect();
 ?>
