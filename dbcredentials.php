efb80972abbf935e532357f79d614cc94d359b17
terix2k12 <terixmail@googlemail.com>
2024-08-21 21:19:10 +0200

    add submission pwd

---
+++ dbcredentials.php
@@ -0,0 +1,11 @@
+<?php
+	/* Keep this file secret :-) */
+	
+	// Docker credentials:
+    $servername = 'db'; 
+    $username = 'devuser';
+    $dbpassword = 'devpass';
+    $dbname = 'test_db';
+
+	$submissionpwd = "foo";
+?>
